0e704b9ecef6d511d3490c4fee8873eb5f20dde1
artyapple <artyapple@gmail.com>
2023-02-04 02:42:13 +0100

    small fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+export default awsmobile;
+import awsmobile from './aws-exports';
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
